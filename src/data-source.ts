08f96a70c3d71120b86f1fc856ae52c3a056d694
premalnayee <premnayee1@gmail.com>
2025-02-11 10:34:34 +0000

    Configure database connection to use environment variables and add SSL support

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,16 +3,20 @@ import { DataSource } from "typeorm";
 import { Course } from "./entities/Course";
 import { Collection } from "./entities/Collection";
 import { User } from "./entities/User";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+// connect to render database
 export const AppDataSource = new DataSource({
+  url: process.env.DATABASE_URL,
+  // port:3306,
   type: "postgres",
-  host: process.env.DB_HOST || "localhost",
-  port: Number(process.env.DB_PORT) || 3306,
-  username: process.env.DB_USERNAME || "postgres",
-  password: process.env.DB_PASSWORD || "password",
-  database: process.env.DB_DATABASE || "osc_coursehub",
   synchronize: false, // Use migrations instead of auto-syncing in production
   logging: false,
   entities: [Course, Collection, User],
-  migrations: ["src/migrations/*.ts"]
+  migrations: ["src/migrations/*.ts"],
+  ssl: {
+    rejectUnauthorized: false,
+  }
 });
